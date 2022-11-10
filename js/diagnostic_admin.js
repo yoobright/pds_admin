4c908dd57218370084656f4878af33a8f591cf57
yoobright <yxj3179@gmail.com>
2022-11-10 14:56:48 +0800

    add search input

---
+++ js/diagnostic_admin.js
@@ -0,0 +1,133 @@
+pds_server = "http://localhost:8089";
+
+layui.use(["form", "table"], function () {
+    var $ = layui.jquery,
+        form = layui.form,
+        table = layui.table;
+
+    const tableData = [];
+    // const blankRowData = {
+    //   create_time: "",
+    //   user_name: "",
+    //   uid: "",
+    //   gender: "",
+    //   age: "",
+    //   recmd: "",
+    //   uuid: ""
+    // };
+    // const tableMax = 1;
+    table.render({
+        elem: "#diagnostic-table",
+        // url: "../api/table.json",
+        url: `${pds_server}/diagnostics`,
+        parseData: function (res) {
+            //res 即为原始返回的数据
+            console.log(res);
+            const data = [];
+            for (const r of res.data) {
+                data.push({
+                    create_time: r.created_at,
+                    user_name: r.patient_basic_info.user_name,
+                    uid: r.patient_basic_info.uid,
+                    gender: r.patient_basic_info.gender,
+                    age: r.patient_basic_info.age,
+                    recmd: r.recmd,
+                    uuid: r.uuid,
+                });
+            }
+            console.log(data);
+
+            return {
+                code: 0,
+                msg: "",
+                count: res.total,
+                data: data,
+            };
+        },
+
+        toolbar: "#toolbarDemo",
+        defaultToolbar: [
+            "filter",
+            "exports",
+            "print",
+            {
+                title: "提示",
+                layEvent: "LAYTABLE_TIPS",
+                icon: "layui-icon-tips",
+            },
+        ],
+        // width: 1600,
+        cols: [
+            [
+                // { type: "checkbox", width: 50 },
+                {
+                    field: "create_time",
+                    width: 320,
+                    title: "提交时间",
+                    sort: true,
+                },
+                { field: "user_name", width: 120, title: "患者名称" },
+                { field: "uid", width: 120, title: "uid" },
+                { field: "gender", width: 80, title: "性别" },
+                { field: "age", width: 80, title: "年龄" },
+                { field: "recmd", width: 400, title: "推荐决策" },
+                { field: "uuid", width: 320, title: "uuid", hide: true },
+                {
+                    title: "操作",
+                    minWidth: 80,
+                    toolbar: "#currentTableBar",
+                    align: "center",
+                    fixed: "right",
+                },
+            ],
+        ],
+        data: tableData,
+        limits: [10, 20, 50, 100],
+        limit: 10,
+        page: { theme: '#1e9fff' },
+        skin: "row",
+        even: true,
+        totalRow: true,
+        id: "testReload"
+    });
+
+    table.on("tool(currentTableFilter)", function (obj) {
+        const data = obj.data;
+        console.log(data);
+
+        // const currentData = layui.table.cache["currentTableId"];
+        const objIdx = parseInt($(obj.tr).attr("data-index"));
+        if (obj.event === "detail") {
+            layer.confirm(`${data.uuid}，详情页面！！!`, function (index) {
+                // obj.del();
+                layer.close(index);
+            });
+        }
+    });
+
+    var $ = layui.$,
+        active = {
+            reload: function () {
+                const name = $("#search-name");
+                if (name.val() === "") {
+                    layer.msg("请输入姓名");
+                    return;
+                }
+
+                //执行重载
+                table.reload("testReload", {
+                    page: {
+                        curr: 1, //重新从第 1 页开始
+                    },
+                    where: {
+                        user_name: name.val(),
+                    },
+                });
+            },
+        };
+
+    $(".demoTable .layui-btn").on("click", function () {
+        var type = $(this).data("type");
+        active[type] ? active[type].call(this) : "";
+    });
+});
