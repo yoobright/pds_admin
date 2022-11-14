c845a63ab7fe58eb12acac9672580bb0ad8084bc
yoobright <yxj3179@gmail.com>
2022-11-14 14:31:04 +0800

    add pain assessment info detail

--- js/diagnostic_detail.js
+++ js/diagnostic_detail.js
@@ -171,9 +171,9 @@ function genTabledata(drugTable) {
     data = [];
     for (const durg of drugTable) {
         data.push({
-            "name": durg.drug_name + durg.spec, 
-            "dose": durg.dose + durg.dose_unit, 
-            "freq": genFreqText(durg.freq, durg.freq_unit), 
+            "name": durg.drug_name + durg.spec,
+            "dose": durg.dose + durg.dose_unit,
+            "freq": genFreqText(durg.freq, durg.freq_unit),
             "duration": genDurationText(durg.duration)
         });
     }
@@ -204,18 +204,120 @@ const adverseReactionDict = {
 }
 
 
-function genAdverseReactionText(strInput) {
+function genTextFromDict(strInput, dict) {
     const inputList = strInput.split(",");
     if (inputList.length === 0) {
         return "";
     }
     res = []
     for (const input of inputList) {
-       if (input in adverseReactionDict) {
-           res.push(adverseReactionDict[input]);
-       }
+        if (input in dict) {
+            res.push(dict[input]);
+        }
     }
     return res.join("、");
+}
+
+const causesDict = {
+    "1": "肿瘤",
+    "2": "肿瘤治疗",
+    "3": "非肿瘤相关性",
+    "-1": "未知"
+}
+
+const characterDict = {
+    "1": "酸痛",
+    "2": "刺痛",
+    "3": "跳痛",
+    "4": "钝痛",
+    "5": "绞痛",
+    "6": "胀痛",
+    "7": "坠痛",
+    "8": "钻顶样痛",
+    "9": "爆裂样痛",
+    "10": "撕裂样痛",
+    "11": "牵拉样痛",
+    "12": "压榨样痛",
+    "13": "放电样痛",
+    "15": "烧灼样痛",
+    "16": "麻木样痛",
+    "17": "刀割样痛",
+    "19": "轻触痛",
+    "23": "无名痛",
+    "24": "隐痛",
+    "25": "尖锐痛"
+}
+
+const bodyPartsDict = {
+    "1": "面部",
+    "2": "头后部",
+    "3": "右上臂（内侧）",
+    "4": "右上臂（外侧）",
+    "5": "左上臂（内侧）",
+    "6": "左上臂（外侧）",
+    "7": "右前臂（内侧）",
+    "8": "右前臂（外侧）",
+    "9": "左前臂（内侧）",
+    "10": "左前臂（外侧）",
+    "11": "右手",
+    "12": "左手",
+    "13": "颈胸部",
+    "14": "颈背部",
+    "15": "腹部（前）",
+    "16": "腹部（后）",
+    "17": "腰部（前）",
+    "18": "腰部（后）",
+    "19": "盆部（右）",
+    "20": "腰骶部（右）",
+    "21": "臀部（右）",
+    "22": "盆部（左）",
+    "23": "腰骶部（左）",
+    "24": "臀部（左）",
+    "25": "大腿（右前）",
+    "26": "大腿（右后）",
+    "27": "大腿（左前）",
+    "28": "大腿（左后）",
+    "29": "小腿（右前）",
+    "30": "小腿（右后）",
+    "31": "小腿（左前）",
+    "32": "小腿（左后）",
+    "33": "右足",
+    "34": "左足"
+}
+
+const aggravatingFactorsDict = {
+    "1": "行走",
+    "2": "活动",
+    "3": "体位变化",
+    "4": "排便",
+    "5": "咳嗽",
+    "6": "进食",
+    "7": "天气",
+    "8": "乏力",
+    "9": "精神因素",
+    "-1": "无"
+}
+
+const relievingFactorsDict = {
+    "1": "服用镇痛药",
+    "2": "环境安静",
+    "3": "光线柔和",
+    "4": "温度适宜",
+    "5": "心理积极",
+    "6": "家人陪伴",
+    "-1": "无"
+}
+
+const breakoutTypeDict = {
+    "1": "与特定活动或事件相关联",
+    "2": "发生在按时给予镇痛药物的剂量间隔结束时",
+    "3": "控制不佳的持续性疼痛",
+    "-1": "无"
+}
+
+const breakoutFreqDict = {
+    "1": "＜3",
+    "2": "≥3",
 }
 
 layui.use(["form", "table"], function () {
@@ -255,6 +357,26 @@ layui.use(["form", "table"], function () {
 
     }
 
+
+    function setPainAssessment(painAssessment) {
+        // pain_score pain_location pain_character pain_duration pain_trigger pain_relief pain_effect pain_effect_other
+        $("[name='causes']").val(
+            genTextFromDict(painAssessment.causes, causesDict));
+        $("[name='body_parts']").val(
+            genTextFromDict(painAssessment.body_parts, bodyPartsDict));
+        $("[name='character']").val(
+            genTextFromDict(painAssessment.character, characterDict));
+        $("[name='level']").val(painAssessment.level);
+        $("[name='aggravating_factors']").val(
+            genTextFromDict(painAssessment.aggravating_factors, aggravatingFactorsDict));
+        $("[name='relief_factors']").val(
+            genTextFromDict(painAssessment.relief_factors, relievingFactorsDict));
+        $("[name='breakout_type']").val(
+            genTextFromDict(painAssessment.breakout_type, breakoutTypeDict));
+        $("[name='breakout_freq']").val(
+            genTextFromDict(painAssessment.breakout_freq, breakoutFreqDict));
+    }
+
     function setPrevMedication(prevMedication) {
         const tableData = genTabledata(prevMedication.drug_table);
         table.render({
@@ -278,7 +400,7 @@ layui.use(["form", "table"], function () {
         $("[name='bad_withdrawal']").val(
             toYesNoText(prevMedication.bad_withdrawal));
         $("[name='adverse_reaction']").val(
-            genAdverseReactionText(prevMedication.adverse_reaction));
+            genTextFromDict(prevMedication.adverse_reaction, adverseReactionDict));
         $("[name='adverse_reaction_drugs']").val(
             prevMedication.adverse_reaction_drugs);
     }
@@ -292,6 +414,8 @@ layui.use(["form", "table"], function () {
             console.log(res);
             const basicInfo = res.patient_basic_info;
             setBasicInfo(basicInfo);
+            const painAssessment = res.pain_assessment_info;
+            setPainAssessment(painAssessment);
             const prevMedication = res.prev_medication_info;
             setPrevMedication(prevMedication);
 
